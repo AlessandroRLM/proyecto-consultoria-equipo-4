e780951e54fe279a33e1495152243e364afb5ed7
Benjamin Osses Bagatello <benjaminosses20@gmail.com>
2025-09-01 20:38:24 -0400

    Cambio de carpeta de providers

--- lib/adapter/transporte/in/ui/screens/transport_page.dart
+++ lib/adapter/transporte/in/ui/screens/transport_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import '../providers/transport_reservations_provider.dart';
+import '../../../out/providers/transport_reservations_provider.dart';
 import 'reservation_page.dart';
 import 'transport_calendar_screen.dart';
 
--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:provider/provider.dart';
 import 'adapter/transporte/in/ui/screens/transport_page.dart';
-import 'adapter/transporte/in/ui/providers/transport_reservations_provider.dart';
+import 'adapter/transporte/out/providers/transport_reservations_provider.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
